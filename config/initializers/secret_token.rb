7d2c9c98d472b3abb300bc2799a48b8f4d979351
Stephen Willingham <gowillingham@gmail.com>
2011-03-20 12:42:13 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worshipschedule::Application.config.secret_token = '81c715248825705b2590071c23a0b6061706b3c2ed30334902be109399a55c16e4b86e36c8d160736abb3d8491971d37f08bd8dc87d52dff460a1e1595f9f619'
