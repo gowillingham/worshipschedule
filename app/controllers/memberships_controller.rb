242b9a7052bb0cf737891f8cf7769e01746bd33a
Stephen Willingham <gowillingham@gmail.com>
2011-06-01 08:58:47 -0500

    Fix broken styling on member edit page.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,8 +3,15 @@ class MembershipsController < ApplicationController
   before_filter(:only => 'create') { require_user_for_current_account(params[:user_id]) }
   before_filter { require_team_for_current_account(params[:team_id]) }
   
-  def create
+  def create_all
     
+  end
+  
+  def remove_all
+    
+  end
+  
+  def create
     @membership = Membership.where(:user_id => params[:user_id], :team_id => params[:team_id]).first
     if @membership.nil?
       @membership = Membership.create(:user_id => params[:user_id], :team_id => params[:team_id])
