972673168b6e642cc734187fea759fb6a68076bb
Stephen Willingham <gowillingham@gmail.com>
2012-01-24 17:01:30 -0600

    Add qTip jquery plugin. Get it working on grid.

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,6 +1,9 @@
 class SlotsController < ApplicationController
   before_filter { require_account_or_team_admin(params[:team_id]) }
   before_filter { require_team_for_current_account(params[:team_id]) }
-  before_filter { require_team_member(params[:team_id]) }  
+  before_filter { require_team_member(params[:team_id]) }    
   
+  def create
+    
+  end
 end
