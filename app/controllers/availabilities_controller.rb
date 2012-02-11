e20fd1b6aacead83c9c8228cf3989dba93277d4b
Stephen Willingham <gowillingham@gmail.com>
2012-02-11 09:36:01 -0600

    Implement upsert by conditionally creating the form on the page depending on whether the availability exists or not.

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,26 +1,28 @@
 class AvailabilitiesController < AccountsController
   before_filter { require_team_member params[:team_id] }
 
+  def update
+    @team = Team.find(params[:team_id])
+    @availability = Availability.find(params[:id])
+    @availability.approved = true
+    if params[:free?]
+      @availability.free = true
+    else
+      @availability.free = false
+    end
+
+    if @availability.save
+      respond_to do |format|
+        format.html { redirect_to team_events_url(@team) }
+      end
+    end    
+  end
+
   def create
     @team = Team.find(params[:team_id])
-    
-    # => create a new record or find the existing and update it ..
-    # => perhaps add upsert method to model ..?
-    
-    @availability = Availability.where(
-      'membership_id = ? AND event_id =?', params[:availability][:membership_id], params[:availability][:event_id]
-    ).first
-    
-    if @availability.nil?
-      @availability = Availability.new(params[:availability])
-      @availability.free = true if params[:free?]
-    else
-      if params[:free?]
-        @availability.free = true
-      else
-        @availability.free = false
-      end
-    end
+    @availability = Availability.new(params[:availability])
+    @availability.free = true if params[:free?]
+    @availability.approved = true
     
     if @availability.save
       respond_to do |format|
